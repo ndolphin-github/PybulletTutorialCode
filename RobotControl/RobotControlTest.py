d2c35a91e45a72562367efe954d66986c78dff54
TaehwaHong <ndolphin93@gmail.com>
2023-08-29 18:08:15 +0900

    Added new files and made edits

---
+++ RobotControl/RobotControlTest.py
@@ -0,0 +1,18 @@
+import pybullet as p
+import time
+import pybullet_data
+
+physicsClient = p.connect(p.GUI)
+p.setAdditionalSearchPath(pybullet_data.getDataPath()) 
+p.setPhysicsEngineParameter(numSolverIterations=10)
+p.setTimeStep(1. / 120.)
+RobotID=p.loadURDF("r2d2.urdf", useMaximalCoordinates=True)
+p.configureDebugVisualizer(p.COV_ENABLE_RENDERING, 0)
+p.configureDebugVisualizer(p.COV_ENABLE_GUI, 0)
+p.configureDebugVisualizer(p.COV_ENABLE_TINY_RENDERER, 0)
+
+print(p.getNumJoints(RobotID))
+
+## Base Link(-1) --- link0 -- link1 -- link 2
+## indices =  [0 .. getNumJoints()] 
+
